db2f6bf91783cdf339e5a2ea0ac72d7f00bb385f
Lisa Hamm <lisalhamm@gmail.com>
2015-02-05 09:51:01 -0600

    restart

--- spec/wrap_spec.rb
+++ spec/wrap_spec.rb
@@ -7,14 +7,6 @@ describe "Word wrap" do
   it "should wrap a string shorter than the line length" do
     expect(WordWrap.wrap("hello", 6)).to eq "hello"
   end
-  it "should wrap on a space" do
-    expect(WordWrap.wrap("HELLO HELLO", 7)).to eq "HELLO\nHELLO"
-  end
-  it 'should wrap on multiple spaces' do
-    expect(WordWrap.wrap('HELLO HELLO HELLO', 7)).to eq "HELLO\nHELLO\nHELLO"
-  end
-  it 'should wrap on spaces when the following word extends longer than specified length' do
-    expect(WordWrap.wrap('Hello world hello', 13)).to eq "Hello world\nhello"
-  end
+
 
 end
