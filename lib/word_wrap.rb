fc806ef90c80d8945cd0e75b1c0e7714dc5a749f
Lisa Hamm <lisalhamm@gmail.com>
2015-02-05 14:26:23 -0600

    Working solution

--- lib/word_wrap.rb
+++ lib/word_wrap.rb
@@ -1,5 +1,15 @@
 module WordWrap
   def self.wrap(string, line_length)
-    string
+    return string if string.length <= line_length
+    if string.index(' ') == nil
+      string[0...line_length] + "\n" + wrap(string[line_length..-1].strip, line_length)
+    else
+      space_index = line_length - string[0..line_length].reverse.index(' ')
+      if space_index != line_length
+        string[0...space_index] + "\n" + wrap(string[space_index+1..-1].strip, line_length)
+      else
+        string[0...line_length] + "\n" + wrap(string[line_length..-1].strip, line_length)
+      end
+    end
   end
 end
--- spec/wrap_spec.rb
+++ spec/wrap_spec.rb
@@ -7,6 +7,22 @@ describe "Word wrap" do
   it "should wrap a string shorter than the line length" do
     expect(WordWrap.wrap("hello", 6)).to eq "hello"
   end
-
-
+  it "should wrap a word longer than the line length" do
+    expect(WordWrap.wrap("hello", 3)).to eq "hel\nlo"
+  end
+  it "should wrap a word longer than 2 line lengths" do
+    expect(WordWrap.wrap("hello", 2)).to eq "he\nll\no"
+  end
+  it "should wrap on a space" do
+    expect(WordWrap.wrap("word word", 4)).to eq "word\nword"
+  end
+  it "should wrap on word boundry" do
+    expect(WordWrap.wrap("word word", 6)).to eq "word\nword"
+  end
+  it "should wrap multiple words on the same line" do
+    expect(WordWrap.wrap("word word four", 11)).to eq "word word\nfour"
+  end
+  it "should wrap multiple words on the same line" do
+    expect(WordWrap.wrap("word word four four", 9)).to eq "word word\nfour four"
+  end
 end
