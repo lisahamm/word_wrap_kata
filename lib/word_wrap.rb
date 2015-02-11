16dbd8639c3885a32c8534c4bde46d3a3f96e08a
Lisa Hamm <lisalhamm@gmail.com>
2015-02-11 17:02:55 -0600

    Added breakline and substring methods

--- lib/word_wrap.rb
+++ lib/word_wrap.rb
@@ -6,19 +6,22 @@ class String
   def remainder_from(index)
     self[index..-1]
   end
+
+  def substring(start_index, end_index)
+    self[start_index..end_index]
+  end
 end
 
 module WordWrap
   def self.wrap(string, line_width)
     return string if string.length <= line_width
-    if string[0...line_width].index(" ") != nil
-      space_index = string[0...line_width].last_index_of(" ")
-      string[0...space_index] + "\n" + wrap(string.remainder_from(space_index+1), line_width)
-    elsif string[line_width] == " "
-      string[0...line_width] + "\n" + wrap(string.remainder_from(line_width+1), line_width)
-    else
-      string[0...line_width] + "\n" + wrap(string.remainder_from(line_width), line_width)
-    end
+    substring = string.substring(0, line_width-1)
+    end_index = substring.index(" ") != nil ? substring.last_index_of(" ") : line_width
+    breakline(string, end_index, line_width)
+  end
+
+  def self.breakline(string, end_index, line_width)
+    string[0...end_index] + "\n" + wrap(string.remainder_from(end_index).strip, line_width)
   end
 end
 
