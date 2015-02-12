5af3ba1a244ac0a73bb5b8d8b0707df67644b59b
Lisa Hamm <lisalhamm@gmail.com>
2015-02-12 16:30:05 -0600

    Refactored solution

--- lib/word_wrap.rb
+++ lib/word_wrap.rb
@@ -1,13 +1,13 @@
 class String
   def last_index_of(character)
-    (self.length - 1) - self.reverse.index(character)
+    self.length-1 - self.reverse.index(character)
   end
 
-  def remainder_from(index)
+  def rest_from(index)
     self[index..-1]
   end
 
-  def substring(start_index, end_index)
+  def substring(start_index=0, end_index)
     self[start_index..end_index]
   end
 end
@@ -15,13 +15,13 @@ end
 module WordWrap
   def self.wrap(string, line_width)
     return string if string.length <= line_width
-    substring = string.substring(0, line_width-1)
-    end_index = substring.index(" ") != nil ? substring.last_index_of(" ") : line_width
-    breakline(string, end_index, line_width)
+    substring = string.substring(line_width-1)
+    end_index = has_a_space?(substring) ? substring.last_index_of(" ") : line_width
+    string.substring(end_index-1) + "\n" + wrap(string.rest_from(end_index).strip, line_width)
   end
 
-  def self.breakline(string, end_index, line_width)
-    string[0...end_index] + "\n" + wrap(string.remainder_from(end_index).strip, line_width)
+  def self.has_a_space?(string)
+    string.index(" ") != nil
   end
 end
 
--- spec/wrap_spec.rb
+++ spec/wrap_spec.rb
@@ -1,19 +1,19 @@
 require './lib/word_wrap'
 
 describe WordWrap do
-  it "wraps an empty string" do
+  it "does not wrap an empty string onto a newline" do
     expect(WordWrap.wrap("", 0)).to eq ""
   end
 
-  it "wraps a string shorter than the line width" do
+  it "does not wrap a string shorter than the line width onto a newline" do
     expect(WordWrap.wrap("word", 6)).to eq "word"
   end
 
-  it "wraps a one word string longer than one line width" do
+  it "wraps a string longer than the line length" do
     expect(WordWrap.wrap("word", 2)).to eq "wo\nrd"
   end
 
-  it "wraps a one word string longer than two line widths" do
+  it "wraps a string longer than two line lengths" do
     expect(WordWrap.wrap("hello", 2)).to eq "he\nll\no"
   end
 
@@ -32,6 +32,4 @@ describe WordWrap do
   it "wraps just before word boundary" do
     expect(WordWrap.wrap("word word", 4)).to eq "word\nword"
   end
-
-
 end
